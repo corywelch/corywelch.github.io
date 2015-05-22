461c490768b9eeb0587e37633e0614d4362a39e1
Cory Welch <cory.welch77@gmail.com>
2015-05-21 20:41:08 -0400

    Updated Theme, Created Login Page for Workout Section

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ function navMouseOver(sectionNumber) {
 	for(var i = 1; i <=4; i++){
 		if(i != sectionNumber){
 			$("#section"+i).addClass("hidden");
+			$("#navsection"+i).removeClass("section"+i).addClass("section"+i+"unselected");
 		} else {
 			$("#section"+i).removeClass("hidden");
+			$("#navsection"+i).removeClass("section"+i+"unselected").addClass("section"+i);
 		}
 	}
+}
+
+function navClicked(sectionNumber) {
+	navMouseOver(sectionNumber);
+	$('html,body').animate({
+		scrollTop: $("#section"+sectionNumber).offset().top
+	},1500);
 }
