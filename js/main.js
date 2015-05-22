9cf202ac0fc2d1f291e6ebea20ce20b4b47c1003
Cory Welch <cory.welch77@gmail.com>
2015-05-22 19:25:50 -0400

    Added Login Functions Now Call Server

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-
+function getServerURL() {
+	//return "localhost:7676";
+	return "173.33.147.9:7676"
+}
 
 function navMouseOver(sectionNumber) {
 	for(var i = 1; i <=4; i++){
@@ -17,4 +20,29 @@ function navClicked(sectionNumber) {
 	$('html,body').animate({
 		scrollTop: $("#section"+sectionNumber).offset().top
 	},1500);
+}
+
+function login() {
+	var username = $('#username').val();
+	var password = $('#password').val();
+	var URL = "http://"+ getServerURL() + "/login.php";
+	$.ajax({
+		type: "POST",
+		url: URL,
+		dataType: "text",
+		data: ("name="+username+"&pass="+password),
+		success: function(reply) {
+			if(reply=="success") {
+				alert("Logged In Successfully");
+			} else if(reply=="invalid") {
+				alert("Invalid Credentials");
+			} else {
+				alert("An Error Occurred : " + reply);
+			}
+		},
+		error: function(error){
+			alert(error);
+		}
+	});
+
 }
