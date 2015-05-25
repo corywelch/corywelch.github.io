f1bed658406ad4d8957451685486330276d195ca
Cory Welch <cory.welch77@gmail.com>
2015-05-25 11:26:01 -0400

    Improving getConfig function

--- js/main.js
+++ js/main.js
@@ -3,42 +3,35 @@ var globalConfig;
 var externalIP;
 var externalIPdetermined = false;
 
+function getipSuccess(json){
+	console.log("Connecting From : "+json.ip);
+	externalIP = json.ip.toString();
+	externalIPdetermined = true;
+}
+
 function getip(){
 	$.ajax({
 		url: "http://jsonip.appspot.com/",
 		type: "GET",
 		dataType: "JSON",
-		success: function(json) {
-			console.log("Connecting From : "+json.ip);
-			externalIP = json.ip.toString();
-			externalIPdetermined = true;
-		},
+		success: getipSuccess(response),
 		error: function(json) {
 			console.log("Error with http://jsonip.appspot.com : "+ JSON.stringify(json));
 			externalIPdetermined = false;
+			console.log("Trying another source");
+			$.ajax({
+				url: "http://ipinfo.io",
+				type: "GET",
+				dataType: "JSON",
+				success: getipSuccess(response),
+				error: function(json) {
+					console.log("Error with http://ipinfo.io : "+ JSON.stringify(json));
+					externalIPdetermined = false;
+					userDetLocation();
+				}
+			});
 		}
 	});
-	if(externalIPdetermined){
-		return
-	}
-	console.log("Trying another source");
-	$.ajax({
-		url: "http://ipinfo.io",
-		type: "GET",
-		dataType: "JSON",
-		success: function(json) {
-			console.log("Connecting From : "+json.ip);
-			externalIP = json.ip.toString();
-			externalIPdetermined = true;
-		},
-		error: function(json) {
-			console.log("Error with http://ipinfo.io : "+ JSON.stringify(json));
-			externalIPdetermined = false;
-		}
-	});
-	if(!externalIPdetermined){
-		userDetLocation();
-	}
 }
 
 function userDetLocation() {
@@ -70,7 +63,7 @@ function getConfig(){
 		if(globalConfig.env != "dev"){
 			getip();
 			if(globalConfig.ip == externalIP && externalIPdetermined == true){
-				console.log("Connecting from within Server's Router. Updating Connection Details");
+				console.log("Connecting from within Server's Router.");
 				globalConfig.ip = "192.168.0.204";
 				globalConfig.port = "80";
 			}
